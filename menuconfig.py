258abbf10a314088ecbcd9f9585f3e82aba88753
Ertuğrul TURAN <60829297+ertugrulturan@users.noreply.github.com>
2021-03-11 02:51:17 +0300

    Update menuconfig.py

--- menuconfig.py
+++ menuconfig.py
@@ -2,17 +2,17 @@
 # See http://standards.freedesktop.org/menu-spec/latest/apa.html
 # Format: [ ["category 1", "icon-for-category-1"], ["category 2", "icon-for-category-2"], "..." ]
 cats = [
-    ["Utility",    "applications-accessories.png"],
-    ["Development","applications-development.png"],
-    ["Network",    "applications-internet.png"   ],
-    ["Office",     "applications-office.png"     ],
-    ["Game",       "applications-games.png"      ],
-    ["Science",    "applications-science.png"    ],
-    ["Graphics",   "applications-graphics.png"   ],
-    ["AudioVideo", "applications-multimedia.png" ],
-    ["System",     "applications-system.png"     ],
-    ["Others",     "applications-other.png"      ],
-    ["Settings",   "applications-utilities.png"  ],
+    ["Utility",    ""],
+    ["Development",""],
+    ["Network",    ""   ],
+    ["Office",     ""     ],
+    ["Game",       ""      ],
+    ["Science",    ""    ],
+    ["Graphics",   ""   ],
+    ["AudioVideo", "" ],
+    ["System",     ""     ],
+    ["Others",     ""      ],
+    ["Settings",   ""  ],
     ]
 
 # Labels for categories (in the same order as the variable above)
